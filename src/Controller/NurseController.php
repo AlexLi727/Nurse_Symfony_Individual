b361be312ccf5d485ac2b2c213a9528ac51f46ce
AlexLi727 <alexavililee@gmail.com>
2024-10-07 16:57:42 +0200

    1

--- src/Controller/NurseController.php
+++ src/Controller/NurseController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -15,13 +14,17 @@ class NurseController extends AbstractController
         $nombre = "Alex";
         $pass = "12345678";
         $correcto = false;
-
-        if($_GET["nombre"] == $nombre && $pass == $_GET["pass"]){
+        
+    if(isset($_POST["nombre"]) && isset($_POST["pass"])){
+        if($_POST["nombre"] == $nombre && $pass == $_POST["pass"]){
             $correcto = true;
-            echo "Negrito";
+            echo "Credenciales correctos";
         }else{
-            echo "Blanquito";
+            echo "Credenciales Incorrectos";
         }
+    }else{
+        echo "No se han proporcionado datos suficientes";
+    }
         
         return new Response($correcto, Response::HTTP_OK);
     }
